0c15924a94b597fc6f07dd210b922a022afc064d
nikhilraj012 <nikhilraj@estavel.ai>
2026-01-19 15:52:58 +0530

    feat: add Landing page component with top padding
    - Create new Landing page component at app/landing/page.js
    - Add pt-14 padding-top to accommodate fixed Navbar
    - Import and render Landing component in main Home page

---
+++ app/landing/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='pt-14'>
+        <h1>Landing Page</h1>
+    </div>
+  )
+}
+
+export default page
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Landing from "./landing/page";
 
 export default function Home() {
   return (
    <div>
     <Navbar />
+    <Landing />
    </div>
   );
 }
